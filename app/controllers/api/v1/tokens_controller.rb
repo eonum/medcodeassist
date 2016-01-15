551ef40574cbae757ac6ed83649a8168f56ac7d6
Arun Sittampalam <arun@sittampalam.ch>
2016-01-15 14:14:00 +0100

    Tokens can be extracted from text.

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TokensController < Api::V1::APIController
   def create
-    @result = [{word:"gehen", token:"geh", start: 0, length: 5}]
+    @result = [{word:"gehen", token:"geh", start: 0]
     respond_to do |format|
       format.json { render :json => @result }
     end
