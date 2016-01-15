26af5189e7c733987bb795ece1de2574d982d83a
Arun Sittampalam <arun@sittampalam.ch>
2016-01-15 14:29:40 +0100

    "Tokens" API working.

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TokensController < Api::V1::APIController
   def create
-    @result = [{word:"gehen", token:"geh", start: 0]
+    @result = Token.find_tokens(params[:text])
     respond_to do |format|
       format.json { render :json => @result }
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1, :defaults => {:format => :json} do
-      resources :highlighted_words, :only => [:create]
+      resources :tokens, :only => [:create]
     end
   end
 end
