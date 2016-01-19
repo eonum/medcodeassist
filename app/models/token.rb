7999dadb5a9881c7e98f093bba160dcdf440ee05
Arun Sittampalam <arun@sittampalam.ch>
2016-01-19 18:13:52 +0100

    Min code length is configurable.

--- app/models/token.rb
+++ app/models/token.rb
@@ -30,10 +30,10 @@ class Token
     Token.where(name: stemmed_word).first
   end
 
-  def find_similar_tokens(count=5, prefix=nil)
+  def find_similar_tokens(count=5,  minimal_token_length=1, prefix=nil)
    similar_tokens = []
     Token.all.each do |token|
-      if token.wordvector and token != self and (prefix.nil? or token.name.start_with? prefix) # TODO: Some tokens have wordvector = nil. Find out why. (e.g token "1105" appeears in vocab and in "sentences" but not in vectors... )
+      if token.wordvector and token != self and (prefix.nil? or token.name.start_with? prefix) and token.name.length >= minimal_token_length + prefix.length # TODO: Some tokens have wordvector = nil. Find out why. (e.g token "1105" appeears in vocab and in "sentences" but not in vectors... )
         current_similarity = Measurable.cosine_similarity(self.wordvector, token.wordvector)
         if similar_tokens.size < count
           similar_tokens.push({token: token, similarity: current_similarity})
@@ -50,9 +50,9 @@ class Token
    similar_tokens.collect {|x| {name: x[:token].name, similarity: x[:similarity]}}
   end
 
-  def self.find_similar_codes(code, count, prefix="CODEPREFIX")
+  def self.find_similar_codes(code, count, minimal_code_length=4, prefix="CODEPREFIX")
     token = Token.where(name: prefix+code).first
-    token.find_similar_tokens(count, prefix).collect {|t| {name: t[:name][prefix.length..-1], similarity: t[:similarity]} }
+    token.find_similar_tokens(count, minimal_code_length, prefix).collect {|t| {name: t[:name][prefix.length..-1], similarity: t[:similarity]} }
   end
 
 end
