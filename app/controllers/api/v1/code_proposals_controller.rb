62e95be07da5db367747b344e9260ce4fb411b80
Arun Sittampalam <arun@sittampalam.ch>
2016-02-25 17:02:31 +0100

    Refactored code_proposals_controller and added some error handling to it.

--- app/controllers/api/v1/code_proposals_controller.rb
+++ app/controllers/api/v1/code_proposals_controller.rb
@@ -1,31 +1,47 @@
 class Api::V1::CodeProposalsController < Api::V1::APIController
   def create
-    code_class_from = nil
-    case params[:code_type_from]
+    input_code_class = nil
+    case params[:input_code_type]
       when 'DRG'
-        code_class_from = Drg
+        input_code_class = Drg
       when 'ICD'
-        code_class_from = IcdCode
+        input_code_class = IcdCode
       when 'CHOP'
-        code_class_from = ChopCode
+        input_code_class = ChopCode
     end
 
-    code_class_to = nil
-    case params[:code_type_to]
-      when 'DRG'
-        code_class_to = Drg
-      when 'ICD'
-        code_class_to = IcdCode
-      when 'CHOP'
-        code_class_to = ChopCode
+    if input_code_class.nil?
+      respond_to do |format|
+        format.json { render :json => {error: "You didn't specify an input_code_type."}, status: 400 }
+      end
+      return
     end
 
+    errors = []
+    code = input_code_class.where({code: params[:input_code]}).first
+    if code.nil?
+      respond_to do |format|
+        format.json { render :json => {error: "The input code you specified coulnd't be found."}, status: 400 }
+      end
+      return
+    end
+    
+    get_drgs = params[:get_drgs] == 'true'
+    get_chops = params[:get_chops] == 'true'
+    get_icds = params[:get_icds] == 'true'
 
     @result = {}
-    if code_class_from and code_class_to and params[:count].to_i > 0
-      code = code_class_from.where({code: params[:code]}).first
+    if input_code_class and params[:count].to_i > 0 and (get_drgs or get_chops or get_icds)
       if code
-        @result = code_class_to.find_similar_codes(code, params[:count].to_i)
+        if get_drgs
+          @result[:drgs] = Drg.find_similar_codes(code, params[:count].to_i)
+        end
+        if get_chops
+          @result[:chops] = ChopCode.find_similar_codes(code, params[:count].to_i)
+        end
+        if get_icds
+          @result[:icds] = IcdCode.find_similar_codes(code, params[:count].to_i)
+        end
       end
     end
 
