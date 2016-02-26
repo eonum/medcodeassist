d1cf41af5e646447e94d5cd80d6a3f1223e07ee6
Arun Sittampalam <arun@sittampalam.ch>
2016-02-26 10:55:27 +0100

    It's now possible to add multiple codes to a code proposal query.

--- app/controllers/api/v1/code_proposals_controller.rb
+++ app/controllers/api/v1/code_proposals_controller.rb
@@ -1,47 +1,65 @@
 class Api::V1::CodeProposalsController < Api::V1::APIController
   def create
-    input_code_class = nil
-    case params[:input_code_type]
-      when 'DRG'
-        input_code_class = Drg
-      when 'ICD'
-        input_code_class = IcdCode
-      when 'CHOP'
-        input_code_class = ChopCode
+
+
+    input_code_classes = {}
+    input_code_types = params[:input_code_types] ? params[:input_code_types] : {}
+    input_code_types.each do |key, input_code_type|
+      case input_code_type
+        when 'DRG'
+          input_code_class = Drg
+        when 'ICD'
+          input_code_class = IcdCode
+        when 'CHOP'
+          input_code_class = ChopCode
+      end
+      input_code_classes[key] = input_code_class
+      if input_code_classes.length == 0
+        respond_to do |format|
+          format.json { render :json => {error: "An input_code_type you specified couldn't be found."}, status: 400 }
+        end
+        return
+      end
     end
 
-    if input_code_class.nil?
-      respond_to do |format|
-        format.json { render :json => {error: "You didn't specify an input_code_type."}, status: 400 }
+    codes = {}
+    input_codes = params[:input_codes] ? params[:input_codes] : {}
+    input_codes.each do |key, input_code|
+      code = input_code_classes[key].where({code: input_code}).first
+      if code.nil?
+        respond_to do |format|
+          format.json { render :json => {error: "An input code you specified coulnd't be found."}, status: 400 }
+        end
+        return
       end
-      return
+      codes[key] = code
     end
 
-    errors = []
-    code = input_code_class.where({code: params[:input_code]}).first
-    if code.nil?
-      respond_to do |format|
-        format.json { render :json => {error: "The input code you specified coulnd't be found."}, status: 400 }
+    wordvectors = codes.map {|key, code| code.average_wordvector }
+    sum = nil
+    wordvectors.each do |wordvector|
+      if sum
+        sum = [sum, wordvector].transpose.map{|x| x.reduce :+}
+      else
+        sum = wordvector.dup
       end
-      return
     end
-    
+    average_wordvector = sum.collect {|x| x / sum.length.to_f}
+
     get_drgs = params[:get_drgs] == 'true'
     get_chops = params[:get_chops] == 'true'
     get_icds = params[:get_icds] == 'true'
 
     @result = {}
-    if input_code_class and params[:count].to_i > 0 and (get_drgs or get_chops or get_icds)
-      if code
-        if get_drgs
-          @result[:drgs] = Drg.find_similar_codes(code, params[:count].to_i)
-        end
-        if get_chops
-          @result[:chops] = ChopCode.find_similar_codes(code, params[:count].to_i)
-        end
-        if get_icds
-          @result[:icds] = IcdCode.find_similar_codes(code, params[:count].to_i)
-        end
+    if params[:count].to_i > 0 and (get_drgs or get_chops or get_icds)
+      if get_drgs
+        @result[:drgs] = Drg.find_similar_codes(average_wordvector, params[:count].to_i)
+      end
+      if get_chops
+        @result[:chops] = ChopCode.find_similar_codes(average_wordvector, params[:count].to_i)
+      end
+      if get_icds
+        @result[:icds] = IcdCode.find_similar_codes(average_wordvector, params[:count].to_i)
       end
     end
 
--- app/models/code.rb
+++ app/models/code.rb
@@ -4,24 +4,22 @@ module Code
   end
 
   module ClassMethods
-    def find_similar_codes(input_code, count)
-      if input_code.nil?
+    def find_similar_codes(input_wordvector, count)
+      if input_wordvector.nil?
         return {}
       end
 
       similar_codes = []
       self.all.each do |code|
-        if code != input_code
-          current_similarity = Measurable.cosine_similarity(code.average_wordvector, input_code.average_wordvector)
-          if similar_codes.size < count
+        current_similarity = Measurable.cosine_similarity(code.average_wordvector, input_wordvector)
+        if similar_codes.size < count
+          similar_codes.push({code: code, similarity: current_similarity})
+          similar_codes.sort_by!{|t| t[:similarity]}.reverse!
+        else
+          if similar_codes[-1][:similarity] < current_similarity
+            similar_codes.pop
             similar_codes.push({code: code, similarity: current_similarity})
             similar_codes.sort_by!{|t| t[:similarity]}.reverse!
-          else
-            if similar_codes[-1][:similarity] < current_similarity
-              similar_codes.pop
-              similar_codes.push({code: code, similarity: current_similarity})
-              similar_codes.sort_by!{|t| t[:similarity]}.reverse!
-            end
           end
         end
       end
