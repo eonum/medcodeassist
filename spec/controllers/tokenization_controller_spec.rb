7b84456a78d1094f5aca285d8383df7a76698e33
Simon Furrer <simon.furrer@students.unibe.ch>
2016-03-19 19:33:48 +0100

    Added more tokenizations controller tests.
    NOTE: Bad design right now: the 'test similarity of tokens' should be tested in a model test.

--- spec/controllers/tokenization_controller_spec.rb
+++ spec/controllers/tokenization_controller_spec.rb
@@ -2,22 +2,54 @@ require 'spec_helper'
 require 'rails_helper'
 
 
-describe 'TokenizationController' do
+describe Api::V1::TokenizationsController do
 
   before do
-    token = Token.new(:name =>"Mirko",:lang =>'de',:wordvector=>['halll','ouups'])
-    token.save
-    @controller = Api::V1::TokenizationsController.new
+    @token = Token.new(:name =>"Mirko",:lang =>'de',:wordvector=>[0,1])
+    @token2 = Token.new(:name =>"Simon",:lang =>'de',:wordvector=>[0,1])
+    @token3 = Token.new(:name =>"Jiannis",:lang =>'de',:wordvector=>[0,1])
+    @token4 = Token.new(:name =>"Antonis",:lang =>'de',:wordvector=>[0,1])
+    @token5 = Token.new(:name =>"LucienM",:lang =>'de',:wordvector=>[0,1])
+    @token.save
+    @token2.save
+    @token3.save
+    @token4.save
+    @token5.save
   end
 
-  describe 'POST #create' do
-    it 'should be successful' do
+    it 'test controller method create' do
+      expect(@controller.respond_to?(:create)).to be(true)
+      get :create, {:format => :json, text: "Mirko, Simon, Jiannis, Antonis, LucienM"}
 
-      post :create, {:format => :json, text: 'hello'}
-      expect(response).to be_success
-      #expect(response).to must_be_empty
+      parsed_body = JSON.parse(response.body)
+
+      # print parsed_body # uncomment if you wanna look how the parsed_body 2D-array looks like
+
+      expect(parsed_body[0]["word"].eql?("Mirko")).to be(true)
+      expect(parsed_body[0]["token"].eql?("Mirko")).to be(true)
+      expect(parsed_body[0]["pos"]).to be(0)
+
+
+      expect(parsed_body[1]["word"].eql?("Simon")).to be(true)
+      expect(parsed_body[1]["token"].eql?("Simon")).to be(true)
+      expect(parsed_body[1]["pos"]).to be(7)
+
+      expect(parsed_body[2]["word"].eql?("Jiannis")).to be(true)
+      expect(parsed_body[2]["token"].eql?("Jiannis")).to be(true)
+      expect(parsed_body[2]["pos"]).to be(14)
+
+      expect(parsed_body[3]["word"].eql?("Antonis")).to be(true)
+      expect(parsed_body[3]["token"].eql?("Antonis")).to be(true)
+      expect(parsed_body[3]["pos"]).to be(23)
+
+      expect(parsed_body[4]["word"].eql?("LucienM")).to be(true)
+      expect(parsed_body[4]["token"].eql?("LucienM")).to be(true)
+      expect(parsed_body[4]["pos"]).to be(32)
     end
-  end
 
-
+    it 'test similarity of tokens' do
+      expect(Token.count).to be(5)
+      expect(@token2.name.eql?("Simon")).to be(true)
+      expect(@token.find_similar_tokens.include?({:token=>"Simon", :similarity=>1.0})).to be(true)
+    end
 end
