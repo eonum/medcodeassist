92698c76e4c845eb1061bf35313dcb402e705855
Arawan7 <simon.furrer@students.unibe.ch>
2016-04-05 14:29:46 +0200

    Added failing api test, adapted routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get 'front_end/index'
-  post '/api/v1/tokenizations/create' # for testing the tokenizations controller
   root 'front_end#index', {:q => "Hello"}
   namespace :api do
     namespace :v1, :defaults => {:format => :json} do
-      resources :tokenization, :only => [:create]
+      resources :tokenizations, :only => [:create]
       resources :synonyms, :only => [:create]
       resources :code_proposals, :only => [:create]
     end
