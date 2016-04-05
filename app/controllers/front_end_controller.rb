4e9343763bfa3b016e39412908a3d1b3fe2839b5
Arawan7 <simon.furrer@students.unibe.ch>
2016-04-05 13:53:07 +0200

    Removed old files and updated install.md (see issue5 on github)

--- app/controllers/front_end_controller.rb
+++ app/controllers/front_end_controller.rb
@@ -1,11 +1,4 @@
 class FrontEndController < ApplicationController
   def index
-    @model = TestModel.new("TestModelName")
-    @text = front_end_params #params[:q]
-  end
-
-  private
-  def front_end_params
-    params.require(:q)
   end
 end
