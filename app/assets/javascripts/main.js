b04c5fdd8b32fd5602c26b14202ea681f3207142
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2016-04-09 19:59:33 +0200

    tab all not working yet.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,56 +10,58 @@
 // });
   $(function() {
 
-    $(".selectable").selectable();
+      $(".selectable").selectable();
 
 
-    $(".selectable").selectable({
-        stop: function() {
-            $(".ui-selected", this).each(function(){
-                this.parentId=this.parentNode.id;
-                $("#"+this.parentId+"Mask, #allListMask").append(this);
-            });
+      $(".selectable").selectable(
+          {
+              stop: function () {
+                  $(".ui-selected", this).each(function () {
+                      this.parentId = this.parentNode.id;
+                      $("#" + this.parentId + "Mask").append(this);
+                  });
 
-        }})});
+              }
 
-     // $("#infoButton").click(function (){
-     //
-     // }
+              // $("#infoButton").click(function (){
+              //
+              // }
 
-      /*
-        {
-            stop: function() {
-                var result = $( "#select-result" ).empty();
-                $( ".ui-selected", this ).each(function() {
-                    var index = $( ".selectable li" ).index( this );
-                    result.append( " #" + ( index + 1 ) );
-                });
-            }
-        }
-        */
-    });
+              /*
+               {
+               stop: function() {
+               var result = $( "#select-result" ).empty();
+               $( ".ui-selected", this ).each(function() {
+               var index = $( ".selectable li" ).index( this );
+               result.append( " #" + ( index + 1 ) );
+               });
+               }
+               }
+               */
+          });
 
 
-    $(".unselectable").selectable({
-        stop: function () {
-            $(".ui-selected", this).each(function () {
-                var id = this.parentId;
-                $("#"+id).prepend(this);
-            });
-        }
-    });
-      
-    $("#analyse").click(function(){
-      // alert("HI");
-      var code="Code aasd";
-      var text= $( "#edit" ).html();
-      var words=["ing", "is","awesome"]
-      words.forEach(function(item) {
-          text=text.split(item).join("<a href='#' class='hight' data-toggle='tooltip' title='"+code +"'>"+item+"</a>");
+      $(".unselectable").selectable({
+          stop: function () {
+              $(".ui-selected", this).each(function () {
+                  var id = this.parentId;
+                  console.log(id);
+                  $("#" + id).prepend(this);
+              });
+          }
       });
-      $("#edit").html(text);
-      // alert("Bye");
-    });
+
+      $("#analyse").click(function () {
+          // alert("HI");
+          var code = "Code aasd";
+          var text = $("#edit").html();
+          var words = ["ing", "is", "awesome"]
+          words.forEach(function (item) {
+              text = text.split(item).join("<a href='#' class='hight' data-toggle='tooltip' title='" + code + "'>" + item + "</a>");
+          });
+          $("#edit").html(text);
+          // alert("Bye");
+      });
 
 
 // $("#edit").keypress(function( event ) {
@@ -88,17 +90,18 @@
 
       // $("#edit").html( $("#edit").html().replace( "Antonis", "<span class='hight'>Antonis</span> " ) )
 
-   //    $( "#edit" )
-   //      .focusin(function() {
+      //    $( "#edit" )
+      //      .focusin(function() {
 
-   // var text= $( "#edit" ).text();
+      // var text= $( "#edit" ).text();
 
-   // text=text.replaceAll("Antonis","<span class='hight'>Antonis</span> ");
+      // text=text.replaceAll("Antonis","<span class='hight'>Antonis</span> ");
 
-   // $("#edit").html(text);
-   //    // $("#edit").html( $("#edit").html().replace( "Antonis", "<span class='hight'>Antonis</span> " ) );
-   //  });
+      // $("#edit").html(text);
+      //    // $("#edit").html( $("#edit").html().replace( "Antonis", "<span class='hight'>Antonis</span> " ) );
+      //  });
 
 
-  // });
+      // });
+  });
  
