579f6aea76fd9243c3f0777152bc25d6e6d22354
GiannisGl <ioannis.glampedakis@students.unibe.ch>
2016-04-10 14:43:44 +0200

    demo data imported. infoButton works but stops after pushing the analyze button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,6 +10,42 @@
 // });
   $(function() {
       var selectedCodes = {};
+      var suggestedCodes = {};
+      suggestedCodes["388410"] = { "_id" : { "$oid" : "56cdb0c49da27e192c003813" }, "code" : "38.84.10", "short_code" : "388410", "text_de" : "Sonstiger chirurgischer Verschluss der thorakalen Aorta"};
+      suggestedCodes["388420"] = { "_id" : { "$oid" : "56cdb0c49da27e192c003814" }, "code" : "38.84.20", "short_code" : "388420", "text_de" : "Sonstiger chirurgischer Verschluss der Aorta abdominalis"};
+      suggestedCodes["388499"] = { "_id" : { "$oid" : "56cdb0c49da27e192c003815" }, "code" : "38.84.99", "short_code" : "388499", "text_de" : "Sonstiger chirurgischer Verschluss der Aorta, sonstige"};
+      suggestedCodes["388500"] = { "_id" : { "$oid" : "56cdb0c49da27e192c003816" }, "code" : "38.85.00", "short_code" : "388500", "text_de" : "Sonstiger chirurgischer Verschluss von anderen thorakalen Gefässen, n.n.bez."};
+      suggestedCodes["388510"] = { "_id" : { "$oid" : "56cdb0c49da27e192c003817" }, "code" : "38.85.10", "short_code" : "388510", "text_de" : "Sonstiger chirurgischer Verschluss von anderen thorakalen Arterien, n.n.bez."};
+      suggestedCodes["388511"] = { "_id" : { "$oid" : "56cdb0c49da27e192c003818" }, "code" : "38.85.11", "short_code" : "388511", "text_de" : "Sonstiger chirurgischer Verschluss der A. subclavia"};
+
+      function updateSuggestedCodes(){
+          var categoryList;
+          $("#mainDiagnosesList").empty();
+          $("#minorDiagnosesList").empty();
+          $("#proceduresList").empty();
+          for(var key in suggestedCodes){
+              if(key=="388410" || key=="388420"){
+                  categoryList = "mainDiagnosesList";
+              }
+              else if(key=="388499" || key=="388500"){
+                  categoryList = "minorDiagnosesList";
+              }
+              else{
+                  categoryList = "proceduresList";
+              }
+              var infoButton = "<button class='infoButton'><img src='http://icons.iconarchive.com/icons/danrabbit/elementary/32/Button-info-icon.png'></button>"
+              $("#"+categoryList).append("<li class='list-group-item' id='"+key+"' data-category='"+categoryList+"'>"+suggestedCodes[key].code+": "+suggestedCodes[key].text_de+infoButton+"</li>")
+          }
+      };
+
+      function hideSelectedCodes(){
+          for(var key in selectedCodes){
+              $(".codeList li").remove("#"+key);
+          }
+      };
+
+      updateSuggestedCodes();
+      hideSelectedCodes();
 
       $(".selectable").selectable();
 
@@ -19,10 +55,12 @@
               stop: function () {
                   $(".ui-selected", this).each(function () {
                       // add the code to allListMask and the appropriate tab-list in code Mask
+                      var id = this.id;
                       var category = this.parentNode.id;
                       this.setAttribute("data-category", category);
                       $("#" + category + "Mask, #allListMask").append(this);
-                      selectedCodes[this.id] = {code: this.id, description: this.description, category: this.category};
+                      selectedCodes[id] = suggestedCodes[id];
+                      selectedCodes[id].category = category;
                   });
               }
       });
@@ -44,17 +82,17 @@
       });
 
       $(".infoButton").click(function (){
-          var codeId = this.parentNode.id;
+          var id = this.parentNode.id;
+          var codeId = suggestedCodes[id].code;
           console.log("info id: "+codeId);
           $("#infoCode").empty().append(codeId);
           $("#infoSynonyms").empty().append("synonyms of code "+codeId);
-          $("#infoDescription").empty().append("Description of code "+codeId);
-          $("#infoDescription").append("<br>bla<br>bla<br>bla<br>bla");
+          $("#infoDescription").empty().append("Description of code "+codeId+":<br>");
+          $("#infoDescription").append(suggestedCodes[id].text_de);
 
       });
 
       $("#analyse").click(function () {
-          // alert("HI");
           var code = "Code aasd";
           var text = $("#edit").html();
           var plainText = $("#edit").text();
@@ -70,11 +108,9 @@
               text = text.split(item).join("<a href='#' class='hight' data-toggle='tooltip' title='" + code + "'>" + item + "</a>");
           });
           $("#edit").html(text);
-          // alert("Bye");
-          for(var key in selectedCodes){
-              console.log("code: "+selectedCodes[key].code);
-              $("#codeList #"+selectedCodes[key].code).hide();
-          }
+
+          updateSuggestedCodes();
+          hideSelectedCodes();
       });
 
 
--- app/controllers/front_end_controller.rb
+++ app/controllers/front_end_controller.rb
@@ -6,16 +6,6 @@ require 'httparty'
   end
 
   def analyse
-    render "index";
-
-    text = params[:text_field].gsub!(/[^0-9A-Za-z]/, ' ')
-
-    body = HTTParty.post("http://pse4.inf.unibe.ch/api/v1/tokenizations", { :query => {:text => text} } )
-    parsed_body =  JSON.parse(body.body)
-
-    parsed_body.each do |element|
-          puts element["token"]
-    end
   end
 
 end
