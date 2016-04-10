162cda702710a156e16deb2742e0ffc8f79b1488
Arawan7 <simon.furrer@students.unibe.ch>
2016-04-10 11:46:34 +0200

    Merged front-end into development.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,35 +9,72 @@
 //   });
 // });
   $(function() {
-    $( "#selectable" ).selectable({
-      stop: function() {
-        var result = $( "#select-result" ).empty();
-        $( ".ui-selected", this ).each(function() {
-          var index = $( "#selectable li" ).index( this );
-          result.append( " #" + ( index + 1 ) );
-        });
-      }
-    });
+      var selectedCodes = {};
+
+      $(".selectable").selectable();
 
 
-$("#analyse").click(function(){
-  // alert("HI");
-     var code="Code";
-      var text= $( "#edit" ).text();
+      $(".selectable").selectable({
 
-    $.ajax({
-        url : "/front_end/analyse",
-        type : "post",
-        data : { text_field: JSON.stringify(text) }
-    });
-
-      var words=["ing", "is","awesome"]
-      words.forEach(function(item) {
-       text=text.split(item).join("<a href='#' class='hight data-toggle='tooltip' title="+code+" "+item+">"+item+"</a>");
+              stop: function () {
+                  $(".ui-selected", this).each(function () {
+                      // add the code to allListMask and the appropriate tab-list in code Mask
+                      var category = this.parentNode.id;
+                      this.setAttribute("data-category", category);
+                      $("#" + category + "Mask, #allListMask").append(this);
+                      selectedCodes[this.id] = {code: this.id, description: this.description, category: this.category};
+                  });
+              }
       });
-       $("#edit").html(text);
-  // alert("Bye");
-});
+
+      $(".unselectable").selectable();
+
+      $(".unselectable").selectable({
+
+          stop: function () {
+              $(".ui-selected", this).each(function () {
+                  // add the code first to the appropriate list
+                  var category = this.getAttribute("data-category");
+                  $("#" + category).prepend(this);
+                  // remove it from all tabs in codeMask
+                  $(".codeMaskLists li").remove("#"+this.id);
+                  delete selectedCodes[this.id];
+              });
+          }
+      });
+
+      $(".infoButton").click(function (){
+          var codeId = this.parentNode.id;
+          console.log("info id: "+codeId);
+          $("#infoCode").empty().append(codeId);
+          $("#infoSynonyms").empty().append("synonyms of code "+codeId);
+          $("#infoDescription").empty().append("Description of code "+codeId);
+          $("#infoDescription").append("<br>bla<br>bla<br>bla<br>bla");
+
+      });
+
+      $("#analyse").click(function () {
+          // alert("HI");
+          var code = "Code aasd";
+          var text = $("#edit").html();
+
+          $.ajax({
+              url : "/front_end/analyse",
+              type : "post",
+              data : { text_field: JSON.stringify(text) }
+          });
+
+          var words = ["ing", "is", "awesome"];
+          words.forEach(function (item) {
+              text = text.split(item).join("<a href='#' class='hight' data-toggle='tooltip' title='" + code + "'>" + item + "</a>");
+          });
+          $("#edit").html(text);
+          // alert("Bye");
+          for(var key in selectedCodes){
+              console.log("code: "+selectedCodes[key].code);
+              $("#codeList #"+selectedCodes[key].code).hide();
+          }
+      });
 
 
 // $("#edit").keypress(function( event ) {
@@ -60,23 +97,24 @@ $("#analyse").click(function(){
 //        $("#edit").caret('pos', x);
 //        x=0;
 //    }
- 
+
 //   });
-  
-  
+
+
       // $("#edit").html( $("#edit").html().replace( "Antonis", "<span class='hight'>Antonis</span> " ) )
-    
-   //    $( "#edit" )
-   //      .focusin(function() {
 
-   // var text= $( "#edit" ).text();
+      //    $( "#edit" )
+      //      .focusin(function() {
 
-   // text=text.replaceAll("Antonis","<span class='hight'>Antonis</span> ");
-  
-   // $("#edit").html(text);
-   //    // $("#edit").html( $("#edit").html().replace( "Antonis", "<span class='hight'>Antonis</span> " ) );
-   //  });
+      // var text= $( "#edit" ).text();
+
+      // text=text.replaceAll("Antonis","<span class='hight'>Antonis</span> ");
+
+      // $("#edit").html(text);
+      //    // $("#edit").html( $("#edit").html().replace( "Antonis", "<span class='hight'>Antonis</span> " ) );
+      //  });
 
 
+      // });
   });
  
