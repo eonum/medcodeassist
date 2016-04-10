a56c1d89b85e05276e71f87559309ba16e675fb8
Arawan7 <simon.furrer@students.unibe.ch>
2016-04-10 13:41:54 +0200

    Setting up api calls for every controller.

--- app/controllers/front_end_controller.rb
+++ app/controllers/front_end_controller.rb
@@ -10,12 +10,37 @@ require 'httparty'
 
     text = params[:text_field].gsub!(/[^0-9A-Za-z]/, ' ')
 
-    body = HTTParty.post("http://pse4.inf.unibe.ch/api/v1/tokenizations", { :query => {:text => text} } )
-    parsed_body =  JSON.parse(body.body)
+    tokens = HTTParty.post("http://pse4.inf.unibe.ch/api/v1/tokenizations", { query: {text: "Diagnose:
+- Morbus Hodgkin (03/93)
+  Lokalisation: Lymphknoten mehrerer Regionen (ICD-O 2: C77.8)
+  Klassifikation nach Ann Arbor: 3B
+- Explorative Laparatomie zum Hodgkin-Staging (03/93)
+- Mantelfeldbestrahlung (03/93)
+- COPP (06/93)
+Begleiterkrankungen:
+- insulinpflichtiger Diabetes mellitus (ICD-9 250.-) (gebessert)"} } )
 
-    parsed_body.each do |element|
-          puts element["token"]
+    parsed_tokens =  JSON.parse(tokens.body)
+
+    synonym = HTTParty.post("http://pse4.inf.unibe.ch/api/v1/synonyms", {query: {word: "mellitus", count: "3"}})
+    parsed_synonym = JSON.parse(synonym.body)
+
+    code_proposals = HTTParty.post("http://pse4.inf.unibe.ch/api/v1/code_proposals", {query: {input_codes: ['ICD_E1141'], input_code_types: ['ICD'], get_icds: true, count: 1 }})
+    #parsed_codes = JSON.parse(code_proposals.body)
+    puts code_proposals
+
+    parsed_tokens.each do |element|
+          puts element
     end
+
+    parsed_synonym.each do |element|
+      puts element
+    end
+=begin
+    parsed_codes.each do |element|
+      puts element
+    end
+=end
   end
 
 end
